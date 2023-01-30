8ca14d3ead5e123f9d0959bb9fce2f356a56ed63
StevanC94 <100795878+StevanC94@users.noreply.github.com>
2023-01-30 10:22:52 -0500

    subida de archivos

---
+++ src/test/java/com/bot/prueba/PruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
